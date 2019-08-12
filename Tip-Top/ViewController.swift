54ea638e1fadc33c3c9e11bfe0b54bac41e9374d
jsabrams <jsa10@optimum.net>
2019-08-12 10:26:01 -0400

    Initial Commit

---
+++ Tip-Top/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tip-Top
+//
+//  Created by John Stanton Abrams on 8/12/19.
+//  Copyright © 2019 John Stanton Abrams. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Tip-TopTests/Tip_TopTests.swift
@@ -0,0 +1,34 @@
+//
+//  Tip_TopTests.swift
+//  Tip-TopTests
+//
+//  Created by John Stanton Abrams on 8/12/19.
+//  Copyright © 2019 John Stanton Abrams. All rights reserved.
+//
+
+import XCTest
+@testable import Tip_Top
+
+class Tip_TopTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
